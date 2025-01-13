51c772f31b0c9b43cd0959a66cc1ac511abc7ba2
arojitmondal996e <arojitmondal996@gmail.com>
2025-01-13 19:54:04 +0600

    navbar is working

---
+++ components/Home/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h2 className="overflow-hidden">I am home</h2>
+        </div>
+    );
+};
+
+export default Home;
---
+++ components/Home/Navbar/MobileNav.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MobileNav = () => {
+    return (
+        <div>
+            <h1>i am mobile nav</h1>
+        </div>
+    );
+};
+
+export default MobileNav;
